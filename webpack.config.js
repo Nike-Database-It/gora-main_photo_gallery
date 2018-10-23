c7539558e95a9ea9a5f641049f277442106ca384
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-23 16:58:11 -0700

    Initialize webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './client/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        include: path.join(__dirname, '/client/src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
