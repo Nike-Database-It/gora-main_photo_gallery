c145859063355fc0f85fcc3b29d78c33f8b42ab5
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-24 17:26:24 -0700

    Prepare ids for seeding

--- db/Shoe.js
+++ db/Shoe.js
@@ -3,9 +3,7 @@ const db = require('./connection.js');
 
 const shoeSchema = new mongoose.Schema({
   shoeID: {type: String, unique: true},
-  name: String,
   imageUrls: [String],
-  color: String,
 })
 
 const Shoe = mongoose.model('Shoe', shoeSchema);
