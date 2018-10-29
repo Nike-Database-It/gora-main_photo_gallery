6a36dd875b54aad6c49d06002e2c62d158a6915a
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-29 15:59:56 -0700

    Add margin and padding for correct div dimensions

--- client/src/components/ImageGrid.jsx
+++ client/src/components/ImageGrid.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ImageRow from './ImageRow';
 
 const ImageGrid = ({ imgs }) => (
-  <div className="image-grid">
+  <div className="image-grid pdp-6-up">
     {imgs.map((pair, i) => (
       <ImageRow key={`image_${i + 1}`} pair={pair} />
     ))}
