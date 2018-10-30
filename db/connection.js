70ba918c3057e83e836772c6145e4ca85cca1bb7
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-30 16:56:31 -0700

    Restructure file & folder configuration

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const mongoUri = 'mongodb://localhost/nikeFEC';
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
-db.catch(() => {
-  // console.log(err);
+db.catch((err) => {
+  console.log(err);
 }).then(() => {
-  // console.log('>>>>> Connected to MongoDB...');
+  console.log('>>>>> Connected to MongoDB...');
 });
 
 module.exports = db;
